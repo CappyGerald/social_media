20cf58596dec76c3206bd7d75c87c203d7d7ab88
Gerald Odhiambo <106544962+CappyGerald@users.noreply.github.com>
2024-11-19 12:14:09 +0300

    Started a social media project

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from account.views import user_login
-
+from django.contrib.auth.views import LoginView, LogoutView
+from account.views import dashboard
 urlpatterns = [
-    path('account/', user_login, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', dashboard, name='dashboard'),
 ]
