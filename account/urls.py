c752ce509f43de91b476bfd88bc5231be1922709
Gerald Odhiambo <106544962+CappyGerald@users.noreply.github.com>
2024-11-20 18:31:37 +0300

    mapped views to change, reset & confirm change to the urls paterns

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from account.views import user_login
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView,PasswordChangeDoneView, PasswordResetView
 from account.views import dashboard
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('', dashboard, name='dashboard'),
+    path('password-change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset' ),
 ]
