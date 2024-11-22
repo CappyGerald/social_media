cc64686bd78f3c9fe5eb92172a7ce4d539e7b391
Gerald Odhiambo <106544962+CappyGerald@users.noreply.github.com>
2024-11-22 13:30:43 +0300

    Added registration templates

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView,PasswordChangeDoneView, PasswordResetView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView,PasswordChangeDoneView,\
+     PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView, PasswordResetCompleteView
 from account.views import dashboard
+
+
+
+
+
+
+
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('', dashboard, name='dashboard'),
     path('password-change/', PasswordChangeView.as_view(), name='password_change'),
-    path('password-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
-    path('password_reset/', PasswordResetView.as_view(), name='password_reset' ),
-]
+    path('password-change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset' ), 
+    path('password-change/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/confirm/', PasswordResetConfirmView.as_view(), name='confirm_password_change'),
+    path('password-reset/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    ]
