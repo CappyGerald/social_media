04f5bccf43cdbeeded4602d2b940197c8cbf8dd3
Gerald Odhiambo <106544962+CappyGerald@users.noreply.github.com>
2024-12-03 18:50:46 +0300

    added the static() helper function to serve media files with the Django development server during development

--- social_media/urls.py
+++ social_media/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
 ]
+
+# adding static helper function to serve media files  with django development server during developmwnt
+
+if settings.DEBUG:
+    urlpatterns += static(
+         settings.MEDIA_URL,
+        document_root = settings.MEDIA_ROOT
+    )
