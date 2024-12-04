39ed9191cac443ab14251235e6304fc1b8be7427
Gerald Odhiambo <106544962+CappyGerald@users.noreply.github.com>
2024-12-04 17:11:07 +0300

    added a feature to allow profiles to be created automatically and associated with User model each tim a new user is created

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 #from django.contrib.auth import login, authenticate
-from .forms import LoginForm, UserRegistrationForm
-
+from .forms import LoginForm, UserRegistrationForm, ProfileForm
+from .models import Profile
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -19,7 +19,11 @@ def register(request):
                 user_form.cleaned_data['password']
             )
             new_user.save()
+
+            #  Corresponding profile objects will be automatically created each time a new user is registered
+            Profile.objects.create(user=new_user)
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
+
