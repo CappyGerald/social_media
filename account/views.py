6e0ac0288d239a65462a34364dc126ad9cf0ae1c
Gerald Odhiambo <106544962+CappyGerald@users.noreply.github.com>
2024-12-04 17:24:53 +0300

    Added edit view for user and profile

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 #from django.contrib.auth import login, authenticate
-from .forms import LoginForm, UserRegistrationForm, ProfileForm
+from .forms import LoginForm, UserRegistrationForm, ProfileForm, UserEditForm, ProfileEditForm
 from .models import Profile
 from django.contrib.auth.decorators import login_required
 
@@ -27,3 +27,24 @@ def register(request):
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
 
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(
+                                 instance=request.user,
+                                 data=request.POST
+                                 )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST.
+            files = request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 
+                  'account/edit_form.html',
+                    {'user_form': user_form,
+                     'profile_form': profile_form})
