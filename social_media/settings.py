f5ffa7c999dda8651652ac00c0f881651ba9accb
Gerald Odhiambo <106544962+CappyGerald@users.noreply.github.com>
2025-01-13 13:03:57 +0300

    Added mysite.com, localhost and 127.0.0.1 to allowed hosts

--- social_media/settings.py
+++ social_media/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = Config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = Config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
